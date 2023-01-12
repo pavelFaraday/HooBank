5d7ccce1ea58452a945d5c5529923aec113c6bfb
pavelFaraday <devstudio.corp@gmail.com>
2023-01-12 19:26:26 +0400

    install Tailwind.css & assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-	return (
-		<div className="App">
-			<h1>Hello</h1>
-		</div>
-	);
-}
+const App = () => {
+	return <div>Hello</div>;
+};
 
 export default App;
