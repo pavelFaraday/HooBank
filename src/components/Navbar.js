60ab7443921e37e8e4136761c044047b47b6556a
pavelFaraday <devstudio.corp@gmail.com>
2023-01-14 22:16:47 +0400

    navbar - step 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div>
+			<nav className="w-full flex py-6 justify-between items-center navbar">
+				<img src={logo} alt="logo" className="w-[124px] h-[32px]" />
+				<ul className="list-none sm:flex hidden justify-end items-center flex-1">
+					{navLinks.map((nav, index) => (
+						<li
+							key={nav.id}
+							className={`font-poppins font-normal cursor-pointer text-[16px] ${
+								index === navLinks.length - 1 ? "mr-0" : "mr-10"
+							} text-white`}
+						>
+							<a href={`#${nav.id}`} className="text-white">
+								{nav.title}
+							</a>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</div>
+	);
 };
 
 export default Navbar;
