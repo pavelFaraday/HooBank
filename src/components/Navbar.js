bfc1510920fbef97bf338d5c31246f833be61c18
pavelFaraday <devstudio.corp@gmail.com>
2023-01-18 21:01:04 +0400

    finish mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+	const [toggle, setToggle] = useState(false);
 	return (
 		<div>
 			<nav className="w-full flex py-6 justify-between items-center navbar">
@@ -21,6 +22,43 @@ const Navbar = () => {
 						</li>
 					))}
 				</ul>
+
+				<div className="sm:hidden flex flex-1 justify-end items-center">
+					<img
+						src={toggle ? close : menu}
+						alt="menu"
+						className="w-[28px] h-[28px] object-fit"
+						onClick={() => {
+							setToggle((prev) => !prev);
+						}}
+					/>
+
+					<div
+						className={`${
+							toggle ? "flex" : "hidden"
+						} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+					>
+						<ul className="list-none flex flex-col justify-end items-center flex-1">
+							{navLinks.map((nav, index) => (
+								<li
+									key={nav.id}
+									className={`font-poppins font-normal cursor-pointer text-[16px] ${
+										index === navLinks.length - 1
+											? "mr-0"
+											: "mb-4"
+									} text-white`}
+								>
+									<a
+										href={`#${nav.id}`}
+										className="text-white"
+									>
+										{nav.title}
+									</a>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
 			</nav>
 		</div>
 	);
