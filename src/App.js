a6a6b33ddb40f78d1d5ad53442657df73b96861c
pavelFaraday <devstudio.corp@gmail.com>
2023-01-21 18:55:32 +0400

    finish HERO component/section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./style";
+
 import {
 	Billing,
 	Business,
@@ -20,15 +21,21 @@ const App = () => (
 				<Navbar />
 			</div>
 		</div>
-		<div className={`bg-primary ${styles.flexStart}`}>
+		<div className={`bg-primary  ${styles.flexStart}`}>
 			<div className={`${styles.boxWidth}`}>
 				<Hero />
 			</div>
 		</div>
 		<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
 			<div className={`${styles.boxWidth}`}>
-				<Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />
-				<Clients /> <CTA /> <Footer />
+				<Stats />
+				<Business />
+				<Billing />
+				<CardDeal />
+				<Testimonials />
+				<Clients />
+				<CTA />
+				<Footer />
 			</div>
 		</div>
 	</div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,54 @@
 import React, { useState } from "react";
-import { close, logo, menu } from "../assets";
+import { logo, close, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
 	return (
-		<div>
-			<nav className="w-full flex py-6 justify-between items-center navbar">
-				<img src={logo} alt="logo" className="w-[124px] h-[32px]" />
-				<ul className="list-none sm:flex hidden justify-end items-center flex-1">
-					{navLinks.map((nav, index) => (
-						<li
-							key={nav.id}
-							className={`font-poppins font-normal cursor-pointer text-[16px] ${
-								index === navLinks.length - 1 ? "mr-0" : "mr-10"
-							} text-white`}
-						>
-							<a href={`#${nav.id}`} className="text-white">
-								{nav.title}
-							</a>
-						</li>
-					))}
-				</ul>
+		<nav className="w-full flex py-6 justify-between items-center navbar">
+			<img src={logo} alt="" className="w-[124px] h-[32px]" />
 
-				<div className="sm:hidden flex flex-1 justify-end items-center">
-					<img
-						src={toggle ? close : menu}
-						alt="menu"
-						className="w-[28px] h-[28px] object-fit"
-						onClick={() => {
-							setToggle((prev) => !prev);
-						}}
-					/>
-
-					<div
-						className={`${
-							toggle ? "flex" : "hidden"
-						} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+			<ul className="list-none sm:flex hidden justify-end items-center flex-1 ">
+				{navLinks.map((nav, index) => (
+					<li
+						key={nav.id}
+						className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${
+							index === navLinks.length - 1 ? "mr-0" : "mr-10"
+						}`}
 					>
-						<ul className="list-none flex flex-col justify-end items-center flex-1">
-							{navLinks.map((nav, index) => (
-								<li
-									key={nav.id}
-									className={`font-poppins font-normal cursor-pointer text-[16px] ${
-										index === navLinks.length - 1
-											? "mr-0"
-											: "mb-4"
-									} text-white`}
-								>
-									<a
-										href={`#${nav.id}`}
-										className="text-white"
-									>
-										{nav.title}
-									</a>
-								</li>
-							))}
-						</ul>
-					</div>
+						<a href={`#${nav.id}`}>{nav.title}</a>
+					</li>
+				))}
+			</ul>
+			<div className="sm:hidden flex flex-1 justify-end item-center">
+				<img
+					src={toggle ? close : menu}
+					alt="menu"
+					className="w-[28px] h-[28] object-fit"
+					onClick={() => setToggle((prev) => !prev)}
+				/>
+				<div
+					className={`${
+						toggle ? "flex" : "hidden"
+					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+				>
+					<ul className="list-none flex flex-col justify-end items-center flex-1 ">
+						{navLinks.map((nav, index) => (
+							<li
+								key={nav.id}
+								className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${
+									index === navLinks.length - 1
+										? "mr-0"
+										: "mb-4"
+								}`}
+							>
+								<a href={`#${nav.id}`}>{nav.title}</a>
+							</li>
+						))}
+					</ul>
 				</div>
-			</nav>
-		</div>
+			</div>
+		</nav>
 	);
 };
 
